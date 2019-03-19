eef2bbbd9c534914ad918033d9a1f5ff679162d5
Churchill <cjprequiroso@gmail.com>
2019-03-19 04:14:14 -0700

    planet question

--- js/planet.js
+++ js/planet.js
@@ -2,7 +2,12 @@
 var play = ["What is the hottest planet?",
  "Which planet is the windiest?",
  "How many moons does Jupiter have?",
- "Which is not a part of the major types of volcanoes?",
+ "How long is one day in Venus?",
+ "What happens to your ffet in space?",
+ "Is a celestal body mostly made of ice, and some dust and rocks.",
+ "",
+ "",
+ "",
  "",
  ];
 
@@ -71,37 +76,142 @@ var play = ["What is the hottest planet?",
 
  				document.getElementById('A').onclick = function() {
  					lives -= 1;
-        if ((lives)==2){
-          alert(z + ", 2 lives left")
-          document.getElementById('heart3').style.visibility = "hidden"
-        }
-        else if ((lives)==1){
-          alert(z + ", 1 life left")
-          document.getElementById('heart2').style.visibility = "hidden"
-        }
-        else if ((lives)==0){
-          window.location.replace('gameover.html')
-        }
+          if ((lives)==2){
+            alert(z + ", 2 lives left")
+            document.getElementById('heart3').style.visibility = "hidden"
+          }
+          else if ((lives)==1){
+            alert(z + ", 1 life left")
+            document.getElementById('heart2').style.visibility = "hidden"
+          }
+          else if ((lives)==0){
+            window.location.replace('gameover.html')
+          }
  				}
  				document.getElementById('B').onclick = function() {
  					document.getElementById('quest').innerHTML = play[3]
-          document.getElementById('A').innerHTML = ""
-          document.getElementById('B').innerHTML = ""
-          document.getElementById('C').innerHTML = ""
+          document.getElementById('A').innerHTML = "243 Earth days"
+          document.getElementById('B').innerHTML = "0.80 Earth days"
+          document.getElementById('C').innerHTML = "11 Earth days"
+
+          document.getElementById('A').onclick = function(){
+            document.getElementById('quest').innerHTML = play[4]
+            document.getElementById('A').innerHTML = "The skin peels off"
+            document.getElementById('B').innerHTML = "Your feet enlarges"
+            document.getElementById('C').innerHTML = "Your bones will crack"
+
+            document.getElementById('A').onclick = function(){
+                document.getElementById('quest').innerHTML = play[5]
+                document.getElementById('A').innerHTML = "Asteroid"
+                document.getElementById('B').innerHTML = "Planet"
+                document.getElementById('C').innerHTML = "Comet"
+
+                document.getElementById('A').onclick = function(){
+                  lives -= 1;
+                  if ((lives)==2){
+                    alert(z + ", 2 lives left")
+                    document.getElementById('heart3').style.visibility = "hidden"
+                  }
+                  else if ((lives)==1){
+                    alert(z + ", 1 life left")
+                    document.getElementById('heart2').style.visibility = "hidden"
+                  }
+                  else if ((lives)==0){
+                    window.location.replace('gameover.html')
+                  }
+                }
+                document.getElementById('B').onclick = function(){
+                  lives -= 1;
+                  if ((lives)==2){
+                    alert(z + ", 2 lives left")
+                    document.getElementById('heart3').style.visibility = "hidden"
+                  }
+                  else if ((lives)==1){
+                    alert(z + ", 1 life left")
+                    document.getElementById('heart2').style.visibility = "hidden"
+                  }
+                  else if ((lives)==0){
+                    window.location.replace('gameover.html')
+                  }
+                }
+                document.getElementById('C').onclick = function(){
+                  document.getElementById('quest').innerHTML = play[6]
+                  document.getElementById('A').innerHTML = ""
+                  document.getElementById('B').innerHTML = ""
+                  document.getElementById('C').innerHTML = ""
+                }
+            }
+            document.getElementById('B').onclick = function(){
+              lives -= 1;
+              if ((lives)==2){
+                alert(z + ", 2 lives left")
+                document.getElementById('heart3').style.visibility = "hidden"
+              }
+              else if ((lives)==1){
+                alert(z + ", 1 life left")
+                document.getElementById('heart2').style.visibility = "hidden"
+              }
+              else if ((lives)==0){
+                window.location.replace('gameover.html')
+              }
+            }
+            document.getElementById('C').onclick = function(){
+              lives -= 1;
+              if ((lives)==2){
+                alert(z + ", 2 lives left")
+                document.getElementById('heart3').style.visibility = "hidden"
+              }
+              else if ((lives)==1){
+                alert(z + ", 1 life left")
+                document.getElementById('heart2').style.visibility = "hidden"
+              }
+              else if ((lives)==0){
+                window.location.replace('gameover.html')
+              }
+            }
+          }
+          document.getElementById('B').onclick = function(){
+            lives -= 1;
+            if ((lives)==2){
+              alert(z + ", 2 lives left")
+              document.getElementById('heart3').style.visibility = "hidden"
+            }
+            else if ((lives)==1){
+              alert(z + ", 1 life left")
+              document.getElementById('heart2').style.visibility = "hidden"
+            }
+            else if ((lives)==0){
+              window.location.replace('gameover.html')
+            }
+          }
+          document.getElementById('C').onclick = function(){
+            lives -= 1;
+            if ((lives)==2){
+              alert(z + ", 2 lives left")
+              document.getElementById('heart3').style.visibility = "hidden"
+            }
+            else if ((lives)==1){
+              alert(z + ", 1 life left")
+              document.getElementById('heart2').style.visibility = "hidden"
+            }
+            else if ((lives)==0){
+              window.location.replace('gameover.html')
+            }
+          }
  				}
  				document.getElementById('C').onclick = function() {
  					lives -= 1;
-        if ((lives)==2){
-          alert(z + ", 2 lives left")
-          document.getElementById('heart3').style.visibility = "hidden"
-        }
-        else if ((lives)==1){
-          alert(z + ", 1 life left")
-          document.getElementById('heart2').style.visibility = "hidden"
-        }
-        else if ((lives)==0){
-          window.location.replace('gameover.html')
-        }
+          if ((lives)==2){
+            alert(z + ", 2 lives left")
+            document.getElementById('heart3').style.visibility = "hidden"
+          }
+          else if ((lives)==1){
+            alert(z + ", 1 life left")
+            document.getElementById('heart2').style.visibility = "hidden"
+          }
+          else if ((lives)==0){
+            window.location.replace('gameover.html')
+          }
  				}
  			}
  			
